6a02bc5315965650020f701e1ba564b116d9ec5b
Andrey Denisenko <francescogacczi@gmail.com>
2018-09-17 22:00:16 +0300

    chenges

---
+++ laf14-4/Graph.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laf14_4
+{
+    class Graph
+    {
+        private readonly int MAX_VERTS = 20;
+        private readonly int INFINITY = 1000000;
+        private Vertex[] vertexList;
+        private int nVerts;
+        private int[,] adjMat;
+        private int[] vertexArr;
+        private List<int[]> list;
+        public Graph()
+        {
+            adjMat = new int[MAX_VERTS, MAX_VERTS];
+            vertexList = new Vertex[MAX_VERTS];
+            nVerts = 0;
+            for (int i = 0; i < MAX_VERTS; i++)
+                for (int j = 0; j < MAX_VERTS; j++)
+                    adjMat[i, j] = INFINITY;
+        }
+        public void addVertex(char lab)
+        {
+            vertexList[nVerts++] = new Vertex(lab);
+        }
+        public void addEdge(int start, int end, int weight)
+        {
+            adjMat[start, end] = weight;
+        }
+    }
+}
---
+++ laf14-4/Vertex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laf14_4
+{
+    class Vertex
+    {
+        public char lable;
+        public bool isInTree;
+        public Vertex(char lab)
+        {
+            lable = lab;
+            isInTree = false;
+        }
+    }
+}
